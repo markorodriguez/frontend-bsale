99e3bc32f223aabbe29c2fd6e857f24577fdfef6
unknown <marko.rodriguez@hotmail.es>
2022-06-06 17:09:05 -0500

    pagination added

--- src/index.js
+++ src/index.js
@@ -27,35 +27,30 @@ const fetchData = async () => {
   const resProd = await fetch("http://localhost:5000/products");
   const dataProd = await resProd.json();
   dataProd.map((product) => products.push(product));
-  
-  tmpArray = dataProd
+
+  tmpArray = dataProd;
 
   const resCat = await fetch("http://localhost:5000/categories");
   const dataCat = await resCat.json();
   dataCat.map((category) => categories.push(category));
 
-  const productsItems = renderProducts(dataProd)
-  gridProducts.innerHTML = productsItems.join("");
+  insertPagination(dataProd);
 
-  const categoriesItems = renderSideBar(dataCat)
+  const categoriesItems = renderSideBar(dataCat);
   categoryList.innerHTML = categoriesItems.join("");
 
-  const dropdownItems = renderDropdown(dataCat)
+  const dropdownItems = renderDropdown(dataCat);
   dropOptions.innerHTML = dropdownItems.join("");
-  
 };
-
 
 const filterData = (params) => {
   const filteredCat = categories.filter((category) => category.id == params);
   const filteredProducts = products.filter(
     (product) => product.category == filteredCat[0].id
   );
-  tmpArray = filteredProducts
-  const filteredItems = renderProducts(filteredProducts);
-  gridProducts.innerHTML = filteredItems.join("");
+  tmpArray = filteredProducts;
+  insertPagination(tmpArray)
 };
-
 
 const renderProducts = (params) => {
   const items = params.map(
@@ -89,51 +84,106 @@ const renderProducts = (params) => {
     `
   );
 
-  return items
-
-}
+  return items;
+};
 
 const renderSideBar = (params) => {
-  const items = params.map((element)=>` <li onclick="filterData(${element.id})" class="my-[3px] cursor-pointer hover:text-orange-400">${element.name.charAt(0).toUpperCase()+''+element.name.slice(1)}</li>`)
-  return items
-}
+  const items = params.map(
+    (element) =>
+      ` <li onclick="filterData(${element.id
+      })" class="my-[3px] cursor-pointer hover:text-orange-400">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
+      }</li>`
+  );
+  return items;
+};
 
 const renderDropdown = (params) => {
-  const items = params.map((element)=>` <div onclick="filterData(${element.id})" class="dropdown-element">${element.name.charAt(0).toUpperCase()+''+element.name.slice(1)}</div>`)
-  return items
-}
+  const items = params.map(
+    (element) =>
+      ` <div onclick="filterData(${element.id})" class="dropdown-element">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
+      }</div>`
+  );
+  return items;
+};
 
 const resetFilter = () => {
-  const items = renderProducts(products)
-  gridProducts.innerHTML = items.join("");
-}
 
-productSearch.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  
+  insertPagination(products);
+};
+
+productSearch.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
   const foundProducts = await fetch("http://localhost:5000/find-product", {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'}, 
-    body: JSON.stringify({product_name: document.getElementById("product_find").value} )
-  })
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      product_name: document.getElementById("product_find").value,
+    }),
+  });
 
-  const data = await foundProducts.json()
-  const items = renderProducts(data)
-  gridProducts.innerHTML = items.join("");
-
-})
+  const data = await foundProducts.json();
+  insertPagination(data);
+});
 
 const sortProductsAsc = () => {
-  const sortedProducts = [...products].sort((a,b) => a.price - b.price)
-  const items = renderProducts(sortedProducts)
-  gridProducts.innerHTML = items.join("");
-}
+  const sortedProducts = [...tmpArray].sort((a, b) => a.price - b.price);
+  insertPagination(sortedProducts);
+};
 
 const sortProductsDesc = () => {
-  /*
-  const sortedProducts =[...tmpArray].sort((a,b) => b.price - a.price)
-  const items = renderProducts(sortedProducts)
-  gridProducts.innerHTML = items.join("");*/
+  const sortedProducts = [...tmpArray].sort((a, b) => b.price - a.price);
+  insertPagination(sortedProducts);
+};
 
-  console.log(tmpArray)
-}
+const sortByDefault = () => {
+  insertPagination(tmpArray);
+};
+
+const insertPagination = (params) => {
+  $(function () {
+    let container = $("#pagination");
+    container.pagination({
+      dataSource: params,
+      pageSize: 8,
+      callback: function (data, pagination) {
+        var dataHtml = "";
+
+        $.each(data, function (index, item) {
+          dataHtml += `
+                <div class="bg-white overflow-hidden rounded-xl shadow-md border-2 h-auto flex flex-col">
+                <div class="h-full">
+                    <img class="h-full bg-white w-full" src="${item.url_image == "" || item.url_image == null
+              ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDNXhjagJgGqhsSMn1cUPkiaMU5RXD_lg9oUkYwhE6Ko_snuPYOsD18DTF8A0hYDQUbN4&usqp=CAU"
+              : item.url_image
+            }" />
+                </div>
+                <div class="py-4">
+                    <span class="font-semibold">${item.name.toUpperCase()}</span>
+                    <div class="flex w-full mt-4 flex-row items-center justify-around">
+                        <div class="flex flex-col">
+                        ${item.discount == 0
+              ? `<span class="font-semibold">CLP ${item.price - item.discount
+              }</span>`
+              : `<span class="font-semibold line-through text-gray-500">CLP ${item.price
+              }</span>
+                            <span class="font-semibold">CLP ${item.price - item.discount
+              }</span>`
+            }
+                        </div>
+                            <div>
+                                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Comprar</button>
+                            </div>
+                    </div>
+                </div>
+            </div>
+                `;
+        });
+
+        dataHtml += "";
+
+        $("#grid-products").html(dataHtml);
+      },
+    });
+  });
+};
