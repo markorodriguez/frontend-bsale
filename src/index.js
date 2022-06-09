271eaa4b36b5bc0fcbfe3762bdd7832434cdca43
unknown <marko.rodriguez@hotmail.es>
2022-06-08 20:29:47 -0500

    filter server side

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ const categoryList = document.getElementById("category-list");
 const productSearch = document.getElementById("product-search");
 const spinnerContainer = document.getElementById("spinner");
 
-const products = [];
-const categories = [];
+
+const products = []; //Será empleado en caso de que el usuario ingrese un valor sin respuesta en el input de búsqueda
+const categories = []; 
+
+//Arreglo temporal para manejar el ordenamiento de productos
 let tmpArray = [];
 
+//Función para ocultar el menú desplegable si el width de la pantalla es mayor a 768px
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 769) {
     dropdown.style.display = "none";
   }
 });
 
-
-
+//Función para activar u ocultar el menú desplegable 
 burgerButton.addEventListener("click", () => {
   if (dropdown.style.display === "none") {
     dropdown.style.display = "block";
@@ -26,14 +29,17 @@ burgerButton.addEventListener("click", () => {
   }
 });
 
+//https://bsale-markorodriguez.herokuapp.com
+
 const fetchData = async () => {
   const resProd = await fetch("https://bsale-markorodriguez.herokuapp.com/products",{
     headers: {'Access-Control-Allow-Origin': '*', 
     'Access-Control-Allow-Headers': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
   }
   });
-  const dataProd = await resProd.json();
+  const dataProd = await resProd.json(); 
 
+ //Empezar el efecto fade del spinner
   setTimeout( () =>{
     spinnerContainer.classList.add("hidden-loading");
   }, 1500) 
@@ -41,8 +47,9 @@ const fetchData = async () => {
     spinnerContainer.classList.add("hidden");
   }, 2500) 
 
+  
   dataProd.map((product) => products.push(product));
-
+  //Almacenar la información de productos para ser empleada en excepciones
   tmpArray = dataProd;
 
   const resCat = await fetch("https://bsale-markorodriguez.herokuapp.com/categories", {
@@ -50,56 +57,82 @@ const fetchData = async () => {
     'Access-Control-Allow-Headers': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
   }
   });
+
+  
   const dataCat = await resCat.json();
+  //Almacenar la información de las categorías para ser empleada en sidebar y menú dropdown
   dataCat.map((category) => categories.push(category));
 
   insertPagination(dataProd);
 
+  //Insertar las categorías en el menú lateral
   const categoriesItems = renderSideBar(dataCat);
   categoryList.innerHTML = categoriesItems.join("");
 
+  //Insertar las categorías en el menú desplegable
   const dropdownItems = renderDropdown(dataCat);
   dropOptions.innerHTML = dropdownItems.join("");
 };
 
-const filterData = (params) => {
-  const filteredCat = categories.filter((category) => category.id == params);
-  const filteredProducts = products.filter(
-    (product) => product.category == filteredCat[0].id
-  );
-  tmpArray = filteredProducts;
-  insertPagination(tmpArray)
-};
+const filterDataProducts = async (params) => {
+  let isValid = false
+  
+  categories.map((category) => {
+    if (category.id == params) {
+      isValid = true;
+    }
+  })
+  
+  if(isValid){
+    const filteredProducts = await fetch(`https://bsale-markorodriguez.herokuapp.com/filter-product`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        category_id: params
+      })
+    })
 
+    const data = await filteredProducts.json();
+    tmpArray = data;
 
+    insertPagination(data);
+
+  }else{
+    console.log('No existe la categoría', isValid)
+    insertPagination(products); //Mostrar los datos obtenidos en la carga del sitio
+  }
+}
+
+//Renderiza cada elemento del sidebar basado en las categorías obtenidas
 const renderSideBar = (params) => {
   const items = params.map(
     (element) =>
-      ` <li onclick="filterData(${element.id
-      })" class="my-[3px] cursor-pointer hover:text-orange-400">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
+      ` <li onclick="filterDataProducts(${element.id})"  class="my-[3px] cursor-pointer hover:text-orange-400">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
       }</li>`
   );
   return items;
 };
 
+//Renderiza cada elemento del dropdown basado en las categorías obtenidas
 const renderDropdown = (params) => {
   const items = params.map(
     (element) =>
-      ` <div onclick="filterData(${element.id})" class="dropdown-element">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
+      ` <div  onclick="filterDataProducts(${element.id})" class="dropdown-element">${element.name.charAt(0).toUpperCase() + "" + element.name.slice(1)
       }</div>`
   );
   return items;
 };
 
+//Emplea 
 const resetFilter = () => {
-
   insertPagination(products);
 };
 
+//Función para realizar el filtro de productos por nombre
 productSearch.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const foundProducts = await fetch("https://bsale-markorodriguez.herokuapp.com/find-product", {
+  const foundProducts = await fetch("http://localhost:5000/find-product", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -112,7 +145,11 @@ productSearch.addEventListener("submit", async (e) => {
   insertPagination(data);
 });
 
+
+//Funciones para ordenar los productos de forma ascendente y descendente respecto a su precio
+//[...tmpArray] permite emplear el array original sin modificarlo
 const sortProductsAsc = () => {
+
   const sortedProducts = [...tmpArray].sort((a, b) => (a.price - (a.price * a.discount/100)) -( b.price - (b.price * b.discount/100)));
   insertPagination(sortedProducts);
 };
@@ -122,10 +159,12 @@ const sortProductsDesc = () => {
   insertPagination(sortedProducts);
 };
 
+//Función para ordenar los productos según sea consumido por defecto desde el backend
 const sortByDefault = () => {
   insertPagination(tmpArray);
 };
 
+//Función que inserta los productos en la página y agrega la paginación
 const insertPagination = (params) => {
   $(function () {
     let container = $("#pagination");
@@ -135,6 +174,7 @@ const insertPagination = (params) => {
       callback: function (data, pagination) {
         var dataHtml = "";
 
+        //Mapea cada elemento del arreglo que se pasa como parámetro y lo renderizará en el target
         $.each(data, function (index, item) {
           dataHtml += `
                 <div class="bg-white overflow-hidden rounded-xl shadow-md border-2 h-auto flex flex-col">
